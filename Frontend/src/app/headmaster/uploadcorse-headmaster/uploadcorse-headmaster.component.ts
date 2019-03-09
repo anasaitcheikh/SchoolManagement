c80d0c9194e7c82afca35ba44a50e2ac8b60ed42
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2019-03-09 02:53:22 +0100

    feat(backend,course-material): handle course material

--- Frontend/src/app/headmaster/uploadcorse-headmaster/uploadcorse-headmaster.component.ts
+++ Frontend/src/app/headmaster/uploadcorse-headmaster/uploadcorse-headmaster.component.ts
@@ -1,33 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
-import {LoginService} from '../../../services/login.service';
+import { LoginService } from '../../../services/login.service';
 import { Router } from '@angular/router'
-import {CourseMaterialService} from '../../../services/courseMaterial.service';
+import { CourseMaterialService } from '../../../services/courseMaterial.service';
+import { Subscription } from 'rxjs';
+import { CourseMaterial } from '../../../utils/types';
+import { API_SERVER } from '../../../utils/server.conf';
+
+const RESOURCE_NAME = 'course-material';
 
 @Component({
   selector: 'app-uploadcorse-headmaster',
   templateUrl: './uploadcorse-headmaster.component.html',
   styleUrls: ['./uploadcorse-headmaster.component.scss']
 })
-export class UploadcorseHeadmasterComponent implements OnInit {
+export class UploadcorseHeadmasterComponent implements OnInit, OnDestroy {
+  private _uploadSubscription: Subscription;
+  private _getSubscription: Subscription;
+
+  DOWNLOAD_RESSOURCE = `${API_SERVER.BASE_URI}/${RESOURCE_NAME}`;
+
   fileToUpload: File = null;
+  description: string = 'hehe';
+
+  courseMaterials: CourseMaterial[] = [];
+
   constructor(private CourseMaterialService: CourseMaterialService, private LoginService: LoginService, private router: Router) { }
 
   ngOnInit() {
-    if(! this.LoginService.is_loggedin()){
+    if (!this.LoginService.is_loggedin()) {
       this.router.navigate(['login'])
+    }
+    this.getAllCourseMaterial();
+  }
+
+  ngOnDestroy(): void {
+    if (this._getSubscription != null) {
+      this._getSubscription.unsubscribe()
+    }
+
+    if (this._uploadSubscription != null) {
+      this._uploadSubscription.unsubscribe()
     }
   }
 
   handleFileInput(files: FileList) {
     this.fileToUpload = files.item(0);
-}
+  }
 
-uploadFile(form){
-  this.CourseMaterialService.uploadFile(form.description, this.fileToUpload).subscribe(
-    success => console.log('success', success),
-    error =>  console.log('error', error)
-  )
-}
+  uploadFile() {
+    console.log("desc", this.description);
+    this._uploadSubscription = this.CourseMaterialService.uploadFile(this.description, this.fileToUpload).subscribe(
+      success => console.log('success', success),
+      error => console.log('error', error)
+    )
+  }
 
+  getAllCourseMaterial() {
+    this._getSubscription = this.CourseMaterialService.getAllCourseMaterial().subscribe(
+      courseMaterials => {
+        console.log('success', courseMaterials);
+        this.courseMaterials = courseMaterials
+      },
+      error => console.log('error', error)
+    )
+  }
 }
