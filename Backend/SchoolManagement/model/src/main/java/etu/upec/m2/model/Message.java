d64d2dbaa21aafe9ee181c22fee2800bd71b4936
Nouhayla <nouhayla.lamjadli@etu.u-pec.fr>
2019-03-17 12:14:26 +0100

    message

--- Backend/SchoolManagement/model/src/main/java/etu/upec/m2/model/Message.java
+++ Backend/SchoolManagement/model/src/main/java/etu/upec/m2/model/Message.java
@@ -6,6 +6,7 @@
 package etu.upec.m2.model;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.*;
 
 /**
@@ -16,9 +17,60 @@ import javax.persistence.*;
 public class Message implements Serializable {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    protected Long id;
+    private Long id;
 
-    
     private String msg;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateAndTime;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_sender", referencedColumnName = "id")
+    private User sender;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_recipient", referencedColumnName = "id")
+    private User recipient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Date getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(Date dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+    
     
 }
