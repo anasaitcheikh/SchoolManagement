b0f497e175bb1e4442c183a72cdbb0584af91051
anas ait cheikh <aitcheikhanas@gmail.com>
2019-03-17 20:35:24 +0100

    student/user services

--- Frontend/src/services/student.service.ts
+++ Frontend/src/services/student.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import {API_SERVER} from '../utils/server.conf';
 @Injectable()
 export class StudentService {
 
   constructor(private  http: HttpClient) { }
+
+  showProfile(token){
+      const req = this.http.get(API_SERVER.BASE_URI +token);
+      return req;
+}
+
+showTimetable(token){
+  const req = this.http.get(API_SERVER.BASE_URI +"XXX");
+  return req;
+}
+
+updatePass(token, oldpass,newpass){
+  const req = this.http.put(API_SERVER.BASE_URI + "XXX"+ token, {
+    user: {
+      xx : oldpass,
+      xxx : newpass
+    }
+  })
+  return req;
+}
+
+getResult(token){
+  const req = this.http.get(API_SERVER.BASE_URI+"result/" +token);
+  return req;
+}
+
+
 
 }
--- Frontend/src/services/user.service.ts
+++ Frontend/src/services/user.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { API_SERVER } from '../utils/server.conf';
 @Injectable()
 export class UserService {
 
   constructor(private http: HttpClient) { }
+
+  sendMessage(em : string,rec : string,obj : string,msg : string){
+    const req = this.http.post(API_SERVER.BASE_URI + "XXX", {
+      message: {
+        id_sender: em,
+        id_recipient: rec,
+        object : obj,
+        msg : msg
+      }
+    })
+    return req;
+  }
+
 
 }
