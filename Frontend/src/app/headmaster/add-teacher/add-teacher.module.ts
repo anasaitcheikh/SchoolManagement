e1eaa0d7a5b19977adb54f8c8c0e1a28b2f95381
Doussou Doumbia <doussou.doumbia@etu.u-pec.fr>
2019-03-17 15:40:27 +0100

    add ngModul for variable retreiving

--- Frontend/src/app/headmaster/add-teacher/add-teacher.module.ts
+++ Frontend/src/app/headmaster/add-teacher/add-teacher.module.ts
@@ -4,12 +4,15 @@ import { AddTeacherComponent } from './add-teacher.component';
 import { routing } from './add-teacher.routing';
 import { SharedModule } from '../../shared/shared.module';
 import { NgxEchartsModule } from 'ngx-echarts';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     NgxEchartsModule,
+    FormsModule,
+    ReactiveFormsModule,
     routing
   ],
   declarations: [
