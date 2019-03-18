d7f3822ab62593da343376d4e8028505b41b4b25
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2019-03-18 11:58:12 +0100

    feat(login): handle login and redirection

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -21,7 +21,10 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   private _loginSubscriber: Subscription
 
-  constructor(private loginService: LoginService, private tokenService: TokenService) { }
+  constructor(private loginService: LoginService, 
+              private tokenService: TokenService,
+              private router: Router
+    ) { }
 
   ngOnInit() {
     
@@ -34,8 +37,22 @@ export class LoginComponent implements OnInit, OnDestroy {
     this.loginError = false;
     this._loginSubscriber = this.loginService.login(this.email, this.password)
       .subscribe(
-        token => {
-          this.tokenService.setToken(JSON.parse(JSON.stringify(token)).token)
+        res => {
+          const resBody = JSON.parse(JSON.stringify(res))
+          this.tokenService.setToken(resBody.token)
+          localStorage.setItem('user', JSON.stringify(resBody.user));
+          
+          switch (resBody.user.status){
+            case 'HEADMASTER':
+              this.router.navigate(['headmaster'])
+              break;
+            case 'TEACHER':
+              this.router.navigate(['teacher'])
+              break;
+            case 'STUDENT':
+              this.router.navigate(['student'])
+              break;
+          }
         },
         error => {
           console.log(error);
