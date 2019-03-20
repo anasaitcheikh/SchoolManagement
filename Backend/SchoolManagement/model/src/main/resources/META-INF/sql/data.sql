da0e9444351b018b19155b690d9c3507ca033786
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2019-03-20 12:02:18 +0100

    feat(backend): add load sql script which create a headmaster

---
+++ Backend/SchoolManagement/model/src/main/resources/META-INF/sql/data.sql
@@ -0,0 +1,7 @@
+/**
+ * Author:  hadji
+ * Created: 20 mars 2019
+ */
+
+INSERT INTO `Headmaster`(`id`, `birthDate`, `email`, `firstname`, `lastname`, `password`, `status`) VALUES (1, '1970-07-09', 'headmaster@mail.com', 'headmaster', 'headmaster', 'password', 'HEADMASTER');
+
