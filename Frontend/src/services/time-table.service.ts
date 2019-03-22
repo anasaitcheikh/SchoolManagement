f233061117b87120abdcc6e96e9c0d1b6a621130
anas ait cheikh <aitcheikhanas@gmail.com>
2019-03-22 21:03:44 +0100

    fix services

--- Frontend/src/services/time-table.service.ts
+++ Frontend/src/services/time-table.service.ts
@@ -10,4 +10,9 @@ export class TimeTableService {
   addCourse(course: Course) {
     return this.httpClient.post<Course>(`${API_SERVER.BASE_URI}/${RESOURCE_NAME}`, course);
   }
+
+  getCourseByTeacher(id_t){
+    const req = this.httpClient.get(API_SERVER.BASE_URI + "/course?teacherId="+id_t);
+    return req;
+  }
 }
