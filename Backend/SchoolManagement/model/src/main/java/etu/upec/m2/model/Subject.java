b44e6b7bbbe029e30fdf142d5d9959e598176950
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2019-03-22 12:43:21 +0100

    fix(backend,model): set identity as  id generation strategy

--- Backend/SchoolManagement/model/src/main/java/etu/upec/m2/model/Subject.java
+++ Backend/SchoolManagement/model/src/main/java/etu/upec/m2/model/Subject.java
@@ -18,7 +18,7 @@ import javax.persistence.*;
 })
 public class Subject implements Serializable {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     
     private Long id;
     
