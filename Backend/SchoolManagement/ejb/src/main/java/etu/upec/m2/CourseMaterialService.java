27c2c7eb3b47fb5aefdebaee7e7acded8977b0b3
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2019-03-23 16:22:56 +0100

    feat(backend,coursematerial): upload

--- Backend/SchoolManagement/ejb/src/main/java/etu/upec/m2/CourseMaterialService.java
+++ Backend/SchoolManagement/ejb/src/main/java/etu/upec/m2/CourseMaterialService.java
@@ -6,6 +6,12 @@
 package etu.upec.m2;
 
 import etu.upec.m2.model.CourseMaterial;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -27,16 +33,45 @@ import javax.persistence.TypedQuery;
 public class CourseMaterialService implements ICourseMaterialService {
 
     private static Logger log = Logger.getLogger(CourseMaterialService.class.getName());
+    private static final String FILES_DIRECTORY = System.getProperty("user.dir") + "/";
     
     @PersistenceContext
     EntityManager em;
     
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public Long createCourseMaterial(CourseMaterial newCourseMaterial) {
-        em.persist(newCourseMaterial);
-        return newCourseMaterial.getId();
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Long createCourseMaterial(String name, String description, InputStream inputStream){
+        String fileExtension = name.substring(name.indexOf('.') + 1);
+        String fileName = name.substring(0, name.indexOf('.')) + "-" + LocalDateTime.now().toString();
+        name = fileName + "." +fileExtension;
+        
+        File file = new File(FILES_DIRECTORY + name);
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        OutputStream outputStream;
+        
+        try {
+            outputStream = new FileOutputStream(file);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            inputStream.close();
+            
+            outputStream.flush();
+            outputStream.close();
+            
+            CourseMaterial courseMaterial = new CourseMaterial();
+            courseMaterial.setName(name);
+            courseMaterial.setDescription(description);
+            
+            em.persist(courseMaterial);
+            return courseMaterial.getId();
+        } catch (Exception ex) {
+            System.err.println(ex);
+            return -1L;
+        }
     }
 
     @Override
@@ -52,14 +87,32 @@ public class CourseMaterialService implements ICourseMaterialService {
             return new Long(0);
         }
         
-        courseMaterial.setLink(newCourseMaterial.getLink());
+        courseMaterial.setName(newCourseMaterial.getName());
         em.merge(newCourseMaterial);
         return courseMaterial.getId();
     }
 
     @Override
     public List<CourseMaterial> getAllCourseMaterial() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<CourseMaterial> typedQuery = em.createNamedQuery("getAllCourseMaterials", CourseMaterial.class);
+        return typedQuery.getResultList();
+    }
+    
+     @Override
+    public File downloadCourseMaterial(Long id) {
+        CourseMaterial courseMaterial;
+        try {
+            courseMaterial = em.find(CourseMaterial.class, id);
+             System.err.println("courseMaterial =>>>>>>>>>> " + courseMaterial);
+        }
+        catch(NoResultException e) {
+            System.err.println("erro =>>>>>>>>>>");
+            e.printStackTrace();
+            return null;
+        }
+        
+        File file = new File(FILES_DIRECTORY + courseMaterial.getName());
+        return file;
     }
 
     @Override
